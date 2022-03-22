9266a19b0be1d57f94f4b1b65c535c007fb96775
Teddy Bandama <bandama.teddy@gmail.com>
2022-03-22 12:41:42 +0100

    add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    resources :review, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
