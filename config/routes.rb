9cf8bcd6fc07b6ba29442e61bba6c46c62efad4a
Teddy Bandama <bandama.teddy@gmail.com>
2022-03-22 08:24:44 +0100

    creation model Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    collection do
-      get :top
-    end
-    member do
-      get :chef
-    end
+    resources :review, only: [:new, :create]
   end
 end
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #collection do
+    #get :top
+  #end
+  #member do
+    #get :chef
+  #end
+#end
